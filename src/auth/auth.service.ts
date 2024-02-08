30a4fa14fa109dd317291f8c61c1f053152288ae
michelxd456 <123332926+MichelBalvinOrmedo1@users.noreply.github.com>
2024-02-07 20:02:58 -0500

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 export class AuthService {
     private readonly clientId = '751902293045624';
     private readonly clientSecret = '00a486c16ebc9243a199f671c0a7affe';
-    private readonly redirectUri = 'https://pruebaapifacebook.onrender.com/auth/instagram/redirect';
+    private readonly redirectUri = 'https://pruebaapinest.onrender.com/auth/instagram/redirect';
 
     async getInstagramAuthUrl(): Promise<string> {
         const authUrl = `https://api.instagram.com/oauth/authorize?client_id=${this.clientId}&redirect_uri=${this.redirectUri}&response_type=code`;
